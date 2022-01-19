bc1a47d6e29d977227df3c8ba1c2b21b1fa0cae2
Júlio Pereira Machado <julioapm@gmail.com>
2022-01-19 17:30:57 -0300

    código inicial

---
+++ src/main/java/julioapm/springresttestes/SpringresttestesApplication.java
@@ -0,0 +1,13 @@
+package julioapm.springresttestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringresttestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringresttestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/julioapm/springresttestes/SpringresttestesApplicationTests.java
@@ -0,0 +1,13 @@
+package julioapm.springresttestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringresttestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
